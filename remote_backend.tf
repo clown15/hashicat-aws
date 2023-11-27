5d6710c7ea62740a6acd9ffa20f70185e24a658d
Your Name <you@example.com>
2023-11-27 04:13:36 +0000

    Added s3 bucket module

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,11 +1,7 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
-<<<<<<< HEAD
     organization = "TFC-instruqt-org"
-=======
-    organization = "instruqt-tfc-org"
->>>>>>> 1761c8db80cab0c071fee975af0931d75660ad3a
+
     workspaces {
       name = "hashicat-aws"
     }
